d48d4a4ee35c1bbd5165bd460cf6cfd69d6ab730
Mateusz Kaduk <mateusz.kaduk@gmail.com>
2024-12-27 14:31:08 +0100

    src/main.rs: Remove stderr silencing.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,11 @@
-// src/main.rs
 use anyhow::Result;
 use cpal::traits::{DeviceTrait, HostTrait};
 
 fn main() -> Result<()> {
-    // Temporarily redirect stderr to /dev/null
-    let stderr_backup = unsafe { libc::dup(2) };
-    let devnull = unsafe { libc::open("/dev/null\0".as_ptr() as *const i8, libc::O_WRONLY) };
-    unsafe { libc::dup2(devnull, 2) };
-
-    // Get devices
     let host = cpal::default_host();
     let devices = host.input_devices()?;
     let device_list: Vec<_> = devices.collect();
 
-    // Restore stderr
-    unsafe {
-        libc::dup2(stderr_backup, 2);
-        libc::close(devnull);
-        libc::close(stderr_backup);
-    }
-
-    // Print devices
     println!("Input Devices:");
     println!("-------------");
     
@@ -36,6 +21,5 @@ fn main() -> Result<()> {
             }
         }
     }
-
     Ok(())
 }
